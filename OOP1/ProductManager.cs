27ff09acd51fef6110c487eafbc41d197d7bc0a7
blntclk <blntclk@windowslive.com>
2021-01-20 23:50:35 +0300

    4. Ders sonu

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //Encapsulation -- 50 tane parametre girmek yerine, özelliklerin tamamını içeren classı 
+        //kümülatif olarak parametre şeklinde göndermemiz durumu
+
+        //void tipi: Git görevi yaptır, ama sonucu ile ilgilenmiyorum.
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + "eklendi.");     
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + "Güncellendi.");
+        }
+
+        //public int Topla(int sayi1, int sayi2) // sonucu geri döndürür. return var
+        //{
+        //    return sayi1 + sayi2;
+        //}
+
+        //public void Topla2(int sayi1, int sayi2) // sonucu geri döndürmez. return kabul etmez.
+        //{
+        //    Console.WriteLine(sayi1 + sayi2);
+        //}
+
+        //public void BiseyYap(int sayi)
+        //{
+        //    sayi = 99;
+        //}
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+            Product product2 = new Product {Id = 2, CategoryId = 5, UnitsInStock = 5,
+                                            ProductName = "Kalem", UnitPrice = 35 }; //Alternatif gösterim yöntemi classlar için
+
+            //Pascal case = Kelimelerin ilk harfi büyük yazılır. Camel Case = Kelimelerin ilk harfleri küçük yazılır.
+            //Case sensitive = Küçük Büyük harf duyarlılığı
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName); // Kamera
+
+            //int sayi = 100;
+            //productManager.BiseyYap(sayi);
+            //Console.WriteLine(sayi); // 100
+
+            //int, double, bool.... değer tip
+            //diziler, class, abstract class, interface .... referans tip
+            // değer tiplerde sadece değer önemlidir. 
+
+        }
+    }
+}
